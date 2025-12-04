14058cb736e275d171e559ef0b187a8801467878
Richard-L4 <rwells945@gmail.com>
2025-12-04 17:03:41 +0000

    final project set up with .env to hide secret key

--- next/settings.py
+++ next/settings.py
@@ -29,7 +29,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 
 
 # Application definition
