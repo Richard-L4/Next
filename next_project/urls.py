2b5c4d1d90ba046ae6392d896da112d35397b7a7
Richard-L4 <rwells945@gmail.com>
2025-12-05 11:45:39 +0000

    set up html pages

--- next_project/urls.py
+++ next_project/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- next_project/views.py
+++ next_project/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "next_project/index.html")
+
+
+def about(request):
+    return render(request, "next_project/about.html")
+
+
+def login(request):
+    return render(request, "next_project/login.html")
+
+
+def logout(request):
+    return render(request, "next_project/logout.html")
+
+
+def register(request):
+    return render(request, "next_project/register.html")
