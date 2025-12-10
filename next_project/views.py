4143ee64d86f2fe84a5637e16a603b80c9aa0f8e
Richard-L4 <rwells945@gmail.com>
2025-12-10 15:12:06 +0000

    change base.html url's and views for better active tab setup

--- next_project/views.py
+++ next_project/views.py
@@ -2,24 +2,27 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "next_project/index.html")
+    return render(request, "next_project/index.html", {'active_tab': 'index'})
 
 
 def detail(request):
-    return render(request, "next_project/detail.html")
+    return render(request,
+                  "next_project/detail.html", {'active_tab': 'detail'})
 
 
 def about(request):
-    return render(request, "next_project/about.html")
+    return render(request, "next_project/about.html", {'active_tab': 'about'})
 
 
 def login(request):
-    return render(request, "next_project/login.html")
+    return render(request, "next_project/login.html", {'active_tab': 'login'})
 
 
 def logout(request):
-    return render(request, "next_project/logout.html")
+    return render(request,
+                  "next_project/logout.html", {'active_tab': 'logout'})
 
 
 def register(request):
-    return render(request, "next_project/register.html")
+    return render(request,
+                  "next_project/register.html", {'active_tab': 'register'})
